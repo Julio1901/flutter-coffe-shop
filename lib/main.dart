6c3c18037d13a14115d450e8b35d6971bfcac37b
Julio Cesar <js.cesar42@gmail.com>
2024-05-09 11:07:47 -0300

    feat: Initial welcome Screen created

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:coffe_shop/src/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WelcomeScreen());
+}
+
+
---
+++ lib/src/screens/welcome_screen.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Image.asset(
+              'assets/images/cup_coffe_background.png',
+              fit: BoxFit.contain,
+            ),
+            Container(
+                color: Colors.blue,
+                height: MediaQuery.of(context).size.height,
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Fall in Love with Coffe in Blissful Delight!',
+                      textDirection: TextDirection.ltr,
+                      style:
+                          TextStyle(fontFamily: 'Sora Semi Bold', fontSize: 32),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'welcome to our coxy coffee corner, wherer every cup is a delighful for you',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontFamily: 'Sora',
+                          fontSize: 14,
+                          color: Color(0xffA2A2A2)),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Sora Semi Bold',
+                              fontSize: 16),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xffC67C4E),
+                            minimumSize: Size(double.infinity, 56)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ))
+          ],
+        )
+      ],
+    ));
+  }
+}
