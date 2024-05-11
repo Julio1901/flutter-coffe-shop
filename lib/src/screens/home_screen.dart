651cd212673651747a6ece55db3b7a8e56ecbab9
Julio Cesar <js.cesar42@gmail.com>
2024-05-11 09:10:58 -0300

    feat: Location text added

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,29 +1,76 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: const SystemUiOverlayStyle(
         statusBarColor: Color(0xff1a1a1a),
       ),
       child: SafeArea(
-        child: Material(  
+        child: Material(
           child: Stack(
             children: [
               Column(
-                 mainAxisSize: MainAxisSize.max,
-                 children: [
+                children: [
                   Container(
+                    width: double.infinity,
                     height: 280,
                     color: Color(0xff1a1a1a),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 24, left: 24),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Location',
+                                      textDirection: TextDirection.ltr,
+                                      style: TextStyle(
+                                          fontFamily: 'Sora',
+                                          fontSize: 12,
+                                          color: Color(0xffA2A2A2)),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Bilzen, Tanjungbalai',
+                                          textDirection: TextDirection.ltr,
+                                          style: TextStyle(
+                                              fontFamily: 'Sora Semi Bold',
+                                              fontSize: 14,
+                                              color: Color(0xffD8D8D8)),
+                                        ),
+                                        SizedBox(width: 4,),
+                                        Image.asset(
+                                                  'assets/icons/icon-arrow-down.png',
+                                                ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   )
-                 ],
+                ],
               ),
             ],
           ),
